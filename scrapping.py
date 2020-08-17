f1419d84c416723fdbd7bdd6cc339c6087acd349
anologicon <pauloraimundi2009@gmail.com>
2020-08-16 23:23:11 -0300

    Adicionando mais campos

--- scrapping.py
+++ scrapping.py
@@ -7,6 +7,19 @@ http  = urllib3.PoolManager()
 Valor = []
 TipoAluguel = []
 Condominio = []
+Area = []
+Quartos = []
+Banheiros = []
+Vagas = []
+Uri = []
+
+def findTextNull(elementFind):
+    elementText = elementFind.text
+
+    if "--" in elementText:
+        return 0
+    
+    return elementText
 
 for page in range(1,5):
 
@@ -26,19 +39,53 @@ for page in range(1,5):
 
         valor.find('span').decompose()
 
-        Valor.append(valor.text.replace(' ','').replace('R$',''))
+        Valor.append(valor.text
+            .replace('.','')
+            .replace(' ','')
+            .replace('R$','')
+            )
 
         condominioFind = row.find('strong', 'js-condo-price')
 
         if(condominioFind):
-            Condominio.append(condominioFind.text.replace('R$',''))
+            Condominio.append(condominioFind.text
+                .replace('R$','')
+                .replace('.','')
+                )
         else :
             Condominio.append(0)
+
+        areaFind = row.find('span', 'property-card__detail-value js-property-card-value property-card__detail-area js-property-card-detail-area')
+
+        Area.append(areaFind.text)
+
+        quartosFind = row.find('span', 'property-card__detail-value js-property-card-value')
+
+        quartosText = findTextNull(quartosFind)
+
+        Quartos.append(quartosText)
+
+        banheirosFind = row.find('span', 'property-card__detail-value js-property-card-value')
+
+        Banheiros.append(findTextNull(banheirosFind))
+
+        vagasFind = row.find('span', 'property-card__detail-value js-property-card-value')
+        
+        Vagas.append(findTextNull(vagasFind))
+
+        uriFind = row.find('a', 'property-card__title js-cardLink js-card-title',  href=True)
+
+        Uri.append("https://www.vivareal.com.br/"+uriFind['href'])
 
 df=pd.DataFrame(Valor,columns=['Valor'])
 
 
 df['TipoAluguel'] = TipoAluguel
 df['Condominio'] = Condominio
+df['Area'] = Area
+df['Quartos'] = Quartos
+df['Banheiros'] = Banheiros
+df['Vagas'] = Vagas
+df['Uri'] = Uri
 
 df.to_csv('./result.csv', index=False)
