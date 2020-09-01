66c476ffab7728c3e5cf5f2bc30c6c0269fc2f1e
anologicon <pauloraimundi2009@gmail.com>
2020-09-01 00:37:52 -0300

    Formatando os bairros

--- etl.py
+++ etl.py
@@ -2,11 +2,13 @@ import pandas as pd;
 import numpy as np;
 
 dataFrame = pd.read_csv('alugueis.csv')
+dfBairros = pd.read_json('Bairro.json')
 
 dataFrame = dataFrame[dataFrame['Endereco'].notna()]
 
 cidades = dataFrame.Cidade.unique()
 
+bairros = list(dfBairros['Bairro'])
 cidades =  list(cidades)
 
 cidades.append('Balneário')
@@ -28,8 +30,20 @@ def cleanEndereco(cidade, endereco):
 
     return endereco
 
-dataFrame['Endereco'] =  dataFrame.apply(lambda x: cleanEndereco(x.Cidade, x.Endereco), axis=1)
+def setBairro(endereco):
+    for bairro in bairros:
+        if endereco is not np.nan:
+            if bairro in endereco:
+                bairro = bairro.replace(' ', '')
+                return bairro
+            
+    return np.nan
 
-dataFrame = dataFrame[dataFrame['Endereco'].notna()]
+dataFrame['Endereco'] =  dataFrame.apply(lambda x: cleanEndereco(x.Cidade, x.Endereco), axis=1)
+dataFrame['Bairro'] =  dataFrame.apply(lambda x: setBairro(x.Endereco), axis=1)
+
+dataFrame = dataFrame[(dataFrame['Bairro'].notna()) & (dataFrame['Endereco'].notna())]
+
+dataFrame['EnderecoFormatado'] = dataFrame.apply(lambda x: x.Bairro+', '+x.Cidade, axis=1)
 
 dataFrame.to_csv('./alugueisFormatados.csv', index=False)
